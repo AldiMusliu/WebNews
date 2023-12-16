f21b067725574338f839e792680d86273b2117d1
AldiMusliu <aldimusliu5@gmail.com>
2023-12-17 00:36:01 +0100

    innital commit

---
+++ WebNewsApplication/Data/Migrations/20231216201421_articlesData.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebNewsApplication.Data.Migrations
+{
+    public partial class articlesData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Contents = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articles", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articles");
+        }
+    }
+}
---
+++ WebNewsApplication/Models/Users.cs
@@ -0,0 +1,18 @@
+﻿namespace WebNewsApplication.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+        
+           
+    
+    }
+}
